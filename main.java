eefe2243f69e17d145f7769c0cde5d65dcc78a0b
afonin.is <anonimmail123@bk.ru>
2023-12-30 01:56:10 +0300

    initial comit

---
+++ main.java
@@ -0,0 +1,19 @@
+package calculator;
+
+import java.util.*;
+
+public class main
+{
+	public static void main (String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		String str = in.nextLine();
+		in.close();
+		System.out.println("Result: " + calculate(str));
+	}
+
+	public static double calculate(String str)
+	{
+		
+	}
+}
