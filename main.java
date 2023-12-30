0158a8f5fccef9509812c3ea5cd32ae220d77bb8
afonin.is <anonimmail123@bk.ru>
2023-12-31 02:13:43 +0300

    30.12.23

--- main.java
+++ main.java
@@ -15,19 +15,91 @@ public class main
 
 	public static Double calculate(String str)
 	{
-		Double res = 0.0;
+		Double res = 0.0, temp;
 		String num = "";
 		Character operation = Character.MIN_VALUE;
 
 		int ascii_num = (int)str.charAt(0);
 
-                        if ((ascii_num < 49 || ascii_num > 57) && ascii_num != 40 && ascii_num != 45)
-                        {
+                if ((ascii_num < 49 || ascii_num > 57) && ascii_num != 40 && ascii_num != 45)
+                {
                                 return Double.NaN;
-                        }
+                }
+
+		ArrayList<Double> num_list = new ArrayList<>();
+		ArrayList<Character> char_list = new ArrayList<>();
 
 		for (int i = 0; i < str.length();)
 		{
+                       	while ( i < str.length() && ascii_num >= 49 && ascii_num <=57 || ascii_num == 46 ||
+				 ascii_num == 45)
+                        {
+                                num += String.valueOf(str.charAt(i));
+				i++;
+								if (i < str.length())
+										ascii_num = (int)str.charAt(i);
+                        }
+                        //System.out.println(num);
+			num_list.add(Double.valueOf(num));
+			num="";
+			if (i < str.length() && (ascii_num == 45 || ascii_num == 43 || ascii_num == 42 ||
+			 ascii_num == 47))
+			{
+				char_list.add(Character.valueOf((char)ascii_num));
+				i++;
+				ascii_num = (int)str.charAt(i);
+			}
+		}
+
+		for (int i = 0; i < char_list.size();)
+		{
+			if (char_list.get(i) == '*')
+			{
+				//System.out.println(num_list.size());
+				int ind = 2*i + 1 >= num_list.size() ? 2*i-1 : 2*i;
+				res = Double.valueOf(num_list.get(ind)) * Double.valueOf(num_list.get(ind+1));
+							num_list.remove(ind);
+							num_list.remove(ind);
+							num_list.add(ind, res);
+							char_list.remove(i);
+			}
+			else if (char_list.get(i) == '/')
+			{
+				res = Double.valueOf(num_list.get(2*i)) / Double.valueOf(num_list.get(2*i + 1));
+							num_list.remove(2*i);
+							num_list.remove(2*i);
+							num_list.add(2*i, res);
+							char_list.remove(i);
+			}
+			else
+			{
+				i++;
+			}
+		}
+
+
+		for (int i = 0; i < char_list.size();)
+		{
+			if (char_list.get(i) == '+')
+			{
+				res = Double.valueOf(num_list.get(2*i)) + Double.valueOf(num_list.get(2*i + 1));
+				num_list.remove(2*i);
+				num_list.remove(2*i);
+				num_list.add(2*i, res);
+				char_list.remove(i);
+			}
+			else
+			{
+				i++;
+			}
+			/*else if (char_list.get(i) == '/')
+			{
+				res = Double.valueOf(num_list.get(2*i)) / Double.valueOf(num_list.get(2*i + 1));
+			}*/
+		}
+
+		/*for (int i = 0; i < str.length();)
+		//{
                         ascii_num = (int)str.charAt(i);
 
 			if (ascii_num >= 49 && ascii_num <=57 || ascii_num == 46 || ascii_num == 45)
@@ -41,7 +113,7 @@ public class main
 					res = Double.valueOf(num);
 					num = "";
 				}
-				operation = (char)ascii_num;
+
 				if (num != "")
 				{
                                 if (operation == '+')
@@ -62,6 +134,7 @@ public class main
 				}
 				}
 				num = "";
+				operation = (char)ascii_num;
 				//System.out.println(operation);
 			}
 
@@ -89,7 +162,7 @@ public class main
 			}
 
 			i++;
-		}
+		}*/
 		return res;
 	}
 }
